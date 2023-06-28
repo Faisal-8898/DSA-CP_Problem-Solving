b221f7460d1fd1d9a36509ad7644d5139a073a2b
Faisal Ahmed <faisalahmed8988@gmail.com>
2023-06-28 12:51:06 +0600

    added lab

---
+++ Linked List/Basics/lab2.cpp
@@ -0,0 +1,195 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+struct node
+{
+    int data;
+    struct node *next;
+};
+
+struct node *head = nullptr;
+struct node *tail = nullptr;
+
+void insertNewNode(int value)
+{
+    struct node *nnode = new node;
+    nnode->data = value;
+    nnode->next = nullptr;
+
+    if (head == NULL)
+    {
+        head = nnode;
+        tail = nnode;
+    }
+    else
+    {
+        tail->next = nnode;
+        tail = nnode;
+    }
+}
+
+void insertInBegining(int value)
+{
+
+    struct node *nnode = new node;
+    nnode->data = value;
+    nnode->next = nullptr;
+
+    if (head == NULL)
+    {
+        head = nnode;
+        tail = nnode;
+    }
+    else
+    {
+        nnode->next = head->next;
+        head = nnode;
+    }
+}
+
+void insertPosition(int value, int position)
+{
+
+    struct node *nnode = new node;
+    struct node *temp, *temp1;
+    nnode->data = value;
+    nnode->next = nullptr;
+
+    if (head == NULL)
+    {
+        head = nnode;
+        tail = nnode;
+    }
+    else
+    {
+        temp = head;
+        int i = 1;
+        while (i < position && temp != NULL)
+        {
+            temp1 = temp;
+            temp = temp->next;
+            i++;
+        }
+        if (temp == NULL)
+        {
+            cout << "WARNING!!! : postion not found " << endl;
+        }
+        else
+        {
+            nnode->next = temp;
+            temp1->next = nnode;
+        }
+    }
+}
+
+void deleteNodeEnd()
+{
+    struct node *temp, *temp1;
+    temp = head;
+    while (temp->next != NULL)
+    {
+        temp1 = temp;
+        temp = temp->next;
+    }
+    temp1->next = nullptr;
+    free(temp);
+}
+
+node *reverse(node *head)
+{
+    node *cur = head;
+    node *pre = nullptr;
+    while (cur != NULL)
+    {
+        node *temp = cur->next;
+        cur->next = pre;
+        pre = cur;
+        cur = temp;
+    }
+    return pre;
+}
+
+void Display(node *&head)
+{
+
+    node *temp = head;
+    cout << "\n\n";
+    cout << "Current linkedlist" << endl;
+    while (temp != NULL)
+    {
+        cout << temp->data << " ";
+        temp = temp->next;
+    }
+}
+
+int main()
+{
+    int t;
+    while (t != 99)
+    {
+        int n;
+        cout << "\n\n\n1. Create linked list" << endl;
+        cout << "2. Insert new node at the begining" << endl;
+        cout << "3. Insert new node at any position" << endl;
+        cout << "4. Delete Node from last position" << endl;
+        cout << "5. Delete Node from begining" << endl;
+        cout << "6. Delete Node from any position" << endl;
+        cout << "7. Reverse linkedlist" << endl;
+        cout << "8. Remove Duplicate from linkedlist" << endl;
+        cout << "0 .display Linked list***" << endl;
+
+        cout << "\n----ENTER A NUMBER TO EXECUTE ANY OPERATION----" << endl;
+        cin >> n;
+        switch (n)
+        {
+        case 0:
+            Display(head);
+            break;
+        case 1:
+            cout << "how many number you want to add?" << endl;
+            int m;
+            cin >> m;
+            while (m--)
+            {
+                cout << "Enter your value to add :" << endl;
+                int n;
+                cin >> n;
+                insertNewNode(n);
+            }
+
+            break;
+        case 2:
+
+            cout << "Enter your value to add :" << endl;
+            int pp;
+            cin >> pp;
+            insertInBegining(pp);
+            break;
+
+        case 3:
+            cout << "Enter your value to add :" << endl;
+            int p, pos;
+            cin >> p;
+            cout << "Enter your position  :" << endl;
+            cin >> pos;
+            insertPosition(p, pos);
+
+            break;
+        case 4:
+            deleteNodeEnd();
+
+            break;
+        case 7:
+        {
+            node *hello = reverse(head);
+            Display(hello);
+        }
+        break;
+        case 99:
+            t = 99;
+            break;
+        default:
+            break;
+        }
+    }
+}
