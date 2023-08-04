ced8809760faf64d35ec9693fe33e7438305c876
Faisal Ahmed <faisalahmed8988@gmail.com>
2023-08-04 22:21:38 +0600

    templete for competitive programming

---
+++ templete.cpp
@@ -0,0 +1,42 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+// Macros for common operations
+#define INF 1e9
+#define MOD 1000000007
+#define ll long long
+#define vi vector<int>
+#define vl vector<ll>
+#define pii pair<int, int>
+#define pll pair<ll, ll>
+#define pb push_back
+#define mp make_pair
+#define all(x) x.begin(), x.end()
+#define F first
+#define S second
+
+// Shortcuts for frequently used code
+#define FOR(i, a, b) for (int i = a; i < b; ++i)
+#define REP(i, n) FOR(i, 0, n)
+#define REV(i, a, b) for (int i = a; i >= b; --i)
+#define REPR(i, n) REV(i, n - 1, 0)
+
+// Function for fast I/O
+void fastIO() {
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+}
+
+// Custom sort function (optional)
+bool compare(const int& a, const int& b) {
+    return a < b;
+}
+
+// Main function
+int main() {
+    fastIO();
+
+    // Your code here
+
+    return 0;
+}
