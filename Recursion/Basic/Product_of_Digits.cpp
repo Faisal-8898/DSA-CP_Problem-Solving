317dfba896b88f2ca60b68a04cc869b720f9e881
Faisal Ahmed <faisalahmed8988@gmail.com>
2023-08-05 12:10:32 +0600

    .

---
+++ Recursion/Basic/Product_of_Digits.cpp
@@ -0,0 +1,51 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+// Macros for common operations
+#define INF 1e9
+#define MOD 1000000007
+#define ll long long
+#define vi vector<int>
+#define vl vector<ll>
+#define pii pair<int, int>
+#define pll pair<ll, ll>
+#define pb push_back
+#define mp make_pair
+#define all(x) x.begin(), x.end()
+#define F first
+#define S second
+
+// Shortcuts for frequently used code
+#define FOR(i, a, b) for (int i = a; i < b; ++i)
+#define REP(i, n) FOR(i, 0, n)
+#define REV(i, a, b) for (int i = a; i >= b; --i)
+#define REPR(i, n) REV(i, n - 1, 0)
+
+// Function for fast I/O
+void fastIO() {
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+}
+
+// Custom sort function (optional)
+bool compare(const int& a, const int& b) {
+    return a < b;
+}
+
+int productDigit(int n){
+ if(n==0){
+    return 1;
+ }
+ else return n%10*productDigit(n/10);
+}
+
+// Main function
+int main() {
+    fastIO();
+
+    int n;
+    cin >> n;
+    cout << productDigit(n);
+
+    return 0;
+}
